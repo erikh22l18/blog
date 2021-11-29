877d3c12429ef917bb762dd0f830c3ff1664cd67
Erikh22l18 <e_2019100825C@uncp.edu.pe>
2021-11-29 12:44:02 -0500

    add geometrics form

--- statics/script.js
+++ statics/script.js
@@ -19,7 +19,9 @@ setInterval(function(){
 var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d')
 
-// funciones para manipular el canvas
+/* funciones para manipular el canvas dibujando figuras 
+ciculos triangulos cuadrados*/
+
 function drawCircle(x, y, r, color){
   ctx.beginPath();
   ctx.arc(x, y, r, 0, 2 * Math.PI);
@@ -31,9 +33,29 @@ function drawLine(x1, y1, x2, y2, color){
   ctx.moveTo(x1, y1)
   ctx.lineTo(x2, y2)
   ctx.strokeStyle = color
+  //cambiar el grosor de linea
+  ctx.lineWidth = 5
   ctx.stroke()
 }
+function drawTriangle(x1, y1, x2, y2, x3, y3, color){
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.lineTo(x3, y3)
+  ctx.fillStyle = color
+  ctx.fill()
+}
 
+function drawSquare(x, y, l, color){
+  ctx.beginPath()
+  ctx.moveTo(x, y)
+  ctx.lineTo(x + l, y)
+  ctx.lineTo(x + l, y + l)
+  ctx.lineTo(x, y + l)
+  ctx.lineTo(x, y)
+  ctx.fillStyle = color
+  ctx.fill()
+}
 
 // dibujar un circulo
 var x = 200
@@ -42,4 +64,28 @@ var r = 50
 var color = 'red'
 drawCircle(x, y, r, color)
 
+// dibujar un cuadrado
+var x = 100
+var y = 100
+var l = 100
+var color = 'yellow'
+drawSquare(x, y, l, color)
+
+// dibujar una linea
+var x1 = 100
+var y1 = 100
+var x2 = 300
+var y2 = 300
+var color = 'blue'
+drawLine(x1, y1, x2, y2, color)
+
+// dibujar un triangulo
+var x1 = 100
+var y1 = 100
+var x2 = 300
+var y2 = 300
+var x3 = 200
+var y3 = 400
+var color = 'green'
+drawTriangle(x1, y1, x2, y2, x3, y3, color)
 
